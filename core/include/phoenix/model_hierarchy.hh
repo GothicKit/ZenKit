feb4986ae3b1bdceb2caf90fe0579b5bc3465af9
Luis Michaelis <private@lmichaelis.de>
2021-05-24 16:31:24 +0200

    core: add model hierarchy parser

---
+++ core/include/phoenix/model_hierarchy.hh
@@ -0,0 +1,37 @@
+// Copyright © 2021 Luis Michaelis
+// Licensed under MIT (https://mit-license.org/).
+#pragma once
+#include <phoenix/detail/stream.hh>
+
+#include <glm/mat4x4.hpp>
+
+#include <tuple>
+#include <vector>
+
+namespace phoenix {
+	struct model_hierarchy_node {
+		s16 parent_index;
+		std::string name;
+		glm::mat4x4 transform;
+	};
+
+	class model_hierachy {
+	public:
+		static model_hierachy parse(reader& in);
+
+		[[nodiscard]] const std::vector<model_hierarchy_node>& nodes() const noexcept { return _m_nodes; }
+
+		[[nodiscard]] std::tuple<glm::vec3, glm::vec3> bbox() const noexcept { return {_m_bbox[0], _m_bbox[1]}; }
+
+		[[nodiscard]] std::tuple<glm::vec3, glm::vec3> collision_bbox() const noexcept { return {_m_collision_bbox[0], _m_collision_bbox[1]}; }
+
+		[[nodiscard]] glm::vec3 root_translation() const noexcept { return _m_root_translation; }
+
+	private:
+		std::vector<model_hierarchy_node> _m_nodes;
+		glm::vec3 _m_bbox[2];
+		glm::vec3 _m_collision_bbox[2];
+		glm::vec3 _m_root_translation;
+	};
+
+}// namespace phoenix
---
+++ core/source/model_hierarchy.cc
@@ -0,0 +1,66 @@
+// Copyright © 2021 Luis Michaelis
+// Licensed under MIT (https://mit-license.org/).
+#include <phoenix/model_hierarchy.hh>
+
+#include <fmt/format.h>
+
+namespace phoenix {
+	enum class hierarchy_chunk {
+		unknown,
+		hierarchy = 0xD100,
+		source = 0xD110,
+		end = 0xD120
+	};
+
+	model_hierachy model_hierachy::parse(reader& in) {
+		model_hierachy hierarchy;
+
+		hierarchy_chunk chunk = hierarchy_chunk::unknown;
+		u32 end = 0;
+		bool end_hierarchy = false;
+
+		do {
+			chunk = static_cast<hierarchy_chunk>(in.read_u16());
+
+			auto length = in.read_u32();
+			end = length + in.tell();
+
+			switch (chunk) {
+				case hierarchy_chunk::hierarchy: {
+					(void) /* version = */ in.read_u32();
+					auto node_count = in.read_u16();
+					hierarchy._m_nodes.resize(node_count);
+
+					for (int i = 0; i < node_count; ++i) {
+						auto& node = hierarchy._m_nodes[i];
+						node.name = in.read_line(false);
+						node.parent_index = in.read_s16();
+						node.transform = glm::mat4 {in.read_vec4(), in.read_vec4(), in.read_vec4(), in.read_vec4()};
+					}
+
+					hierarchy._m_bbox[0] = in.read_vec3();
+					hierarchy._m_bbox[1] = in.read_vec3();
+					hierarchy._m_collision_bbox[0] = in.read_vec3();
+					hierarchy._m_collision_bbox[1] = in.read_vec3();
+					hierarchy._m_root_translation = in.read_vec3();
+
+					(void) /* checksum = */ in.read_u32();
+					break;
+				}
+				case hierarchy_chunk::end:
+					end_hierarchy = true;
+					break;
+				default:
+					break;
+			}
+
+			if (in.tell() != end) {
+				fmt::print(stderr, "warning: model hierarchy: not all data or too much data consumed from section 0x{:X}\n", chunk);
+			}
+
+			in.seek(end);
+		} while (!end_hierarchy);
+
+		return hierarchy;
+	}
+}// namespace phoenix
