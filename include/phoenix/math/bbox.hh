a0f7c0983caff78fddb865cef866115ac04496c0
Luis Michaelis <lmichaelis.all+dev@gmail.com>
2022-04-18 10:31:04 +0200

    project: add `bounding_box` structure

---
+++ include/phoenix/math/bbox.hh
@@ -0,0 +1,23 @@
+// Copyright © 2022 Luis Michaelis
+// Licensed under MIT (https://mit-license.org/).
+#pragma once
+#include <phoenix/detail/buffer.hh>
+
+#include <glm/vec3.hpp>
+
+namespace phoenix {
+	/**
+	 * @brief Represents a simple bounding box (BBox)
+	 */
+	struct bounding_box {
+		glm::vec3 min;
+		glm::vec3 max;
+
+		static bounding_box parse(buffer& in) {
+			bounding_box bbox {};
+			bbox.min = in.get_vec3();
+			bbox.max = in.get_vec3();
+			return bbox;
+		}
+	};
+} // namespace phoenix
