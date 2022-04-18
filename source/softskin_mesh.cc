4e0769afc2f5c1e2f57eb9a66eabe04368971499
Luis Michaelis <lmichaelis.all+dev@gmail.com>
2022-04-18 14:24:47 +0200

    softskin_mesh: update parsers to make use of the new `buffer`

--- source/softskin_mesh.cc
+++ source/softskin_mesh.cc
@@ -5,74 +5,68 @@
 #include <fmt/format.h>
 
 namespace phoenix {
-	enum class softmesh_chunk { unknown, mesh = 0xE100, end = 0xE110 };
+	enum class softmesh_chunk { unknown, header = 0xE100, end = 0xE110, proto = 0xB100, nodes = 0xB1FF };
 
 	softskin_mesh softskin_mesh::parse(buffer& in) {
 		softskin_mesh msh;
-
-		softmesh_chunk chunk = softmesh_chunk::unknown;
-		std::uint32_t end = 0;
+		softmesh_chunk type = softmesh_chunk::unknown;
 		bool end_mesh = false;
 
 		do {
-			chunk = static_cast<softmesh_chunk>(in.get_ushort());
+			type = static_cast<softmesh_chunk>(in.get_ushort());
 
 			auto length = in.get_uint();
-			end = length + in.position();
+			auto chunk = in.extract(length);
 
-			switch (chunk) {
-			case softmesh_chunk::mesh: {
-				(void) /* version = */ in.get_uint();
-				msh._m_mesh = proto_mesh::parse(in);
-
+			switch (type) {
+			case softmesh_chunk::header:
+				(void) /* version = */ chunk.get_uint();
+				break ;
+			case softmesh_chunk::proto:
+				msh._m_mesh = proto_mesh::parse_from_section(chunk);
+				break;
+			case softmesh_chunk::nodes: {
 				// weights
-				{
-					auto weight_buffer_size = in.get_uint();
-					auto weight_buffer_end = in.position() + weight_buffer_size;
+				auto weight_buffer_size = chunk.get_uint();
+				auto weight_buffer_end = chunk.position() + weight_buffer_size;
 
-					msh._m_weights.reserve(in.get_uint());
+				msh._m_weights.reserve(chunk.get_uint());
 
-					if (weight_buffer_size / (sizeof(float) * 4 + sizeof(std::uint8_t)) == msh._m_weights.size()) {
-						fmt::print(
-						    stderr,
-						    "warning: softskin mesh: not all data or too much data consumed from weight section\n");
-					}
-
-					for (std::uint32_t i = 0; i < msh._m_weights.size(); ++i) {
-						auto& weight = msh._m_weights.emplace_back();
-						weight.weight = in.get_float();
-						weight.position = in.get_vec3();
-						weight.node_index = in.get();
-					}
-
-					in.position(weight_buffer_end);
+				if (weight_buffer_size / (sizeof(float) * 4 + sizeof(std::uint8_t)) == msh._m_weights.size()) {
+					fmt::print(stderr, "warning: softskin mesh: not all data from weight section\n");
 				}
 
+				for (std::uint32_t i = 0; i < msh._m_weights.size(); ++i) {
+					auto& weight = msh._m_weights.emplace_back();
+					weight.weight = chunk.get_float();
+					weight.position = chunk.get_vec3();
+					weight.node_index = chunk.get();
+				}
+
+				chunk.position(weight_buffer_end);
+
 				// wedge normals
-				msh._m_wedge_normals.reserve(in.get_uint());
+				msh._m_wedge_normals.reserve(chunk.get_uint());
 
 				for (std::uint32_t i = 0; i < msh._m_wedge_normals.size(); ++i) {
 					auto& normal = msh._m_wedge_normals.emplace_back();
-					normal.normal = in.get_vec3();
-					normal.index = in.get_uint();
+					normal.normal = chunk.get_vec3();
+					normal.index = chunk.get_uint();
 				}
 
 				// nodes
-				msh._m_nodes.resize(in.get_ushort());
+				msh._m_nodes.resize(chunk.get_ushort());
 
 				for (std::uint32_t i = 0; i < msh._m_nodes.size(); ++i) {
-					msh._m_nodes[i] = in.get_int();
+					msh._m_nodes[i] = chunk.get_int();
 				}
 
 				for (std::uint32_t i = 0; i < msh._m_nodes.size(); ++i) {
-					msh._m_bboxes.push_back(obb::parse(in));
+					msh._m_bboxes.push_back(obb::parse(chunk));
 				}
 
-				// hacky bypass for warnings
-				end = in.position();
 				break;
 			}
-
 			case softmesh_chunk::end:
 				end_mesh = true;
 				break;
@@ -80,13 +74,9 @@ namespace phoenix {
 				break;
 			}
 
-			if (in.position() != end) {
-				fmt::print(stderr,
-				           "warning: softskin mesh: not all data or too much data consumed from section 0x{:X}\n",
-				           chunk);
+			if (chunk.remaining() != 0) {
+				fmt::print(stderr, "warning: softskin mesh: not all data consumed from section 0x{:X}\n", type);
 			}
-
-			in.position(end);
 		} while (!end_mesh);
 
 		return msh;
