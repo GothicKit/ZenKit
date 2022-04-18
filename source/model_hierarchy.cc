84fbe0666f246e40f23fe2c51d4e48fe837e3bef
Luis Michaelis <lmichaelis.all+dev@gmail.com>
2022-04-18 13:21:07 +0200

    model_hierarchy: update parser to make use of the new `buffer`

--- source/model_hierarchy.cc
+++ source/model_hierarchy.cc
@@ -10,45 +10,45 @@ namespace phoenix {
 	model_hierachy model_hierachy::parse(buffer& in) {
 		model_hierachy hierarchy;
 
-		hierarchy_chunk chunk = hierarchy_chunk::unknown;
-		std::uint32_t end = 0;
+		hierarchy_chunk type = hierarchy_chunk::unknown;
 		bool end_hierarchy = false;
 
 		do {
-			chunk = static_cast<hierarchy_chunk>(in.get_ushort());
+			type = static_cast<hierarchy_chunk>(in.get_ushort());
 
 			auto length = in.get_uint();
-			end = length + in.position();
+			auto chunk = in.extract(length);
 
-			switch (chunk) {
+			switch (type) {
 			case hierarchy_chunk::hierarchy: {
-				(void) /* version = */ in.get_uint();
-				auto node_count = in.get_ushort();
+				(void) /* version = */ chunk.get_uint();
+				auto node_count = chunk.get_ushort();
 
 				for (int i = 0; i < node_count; ++i) {
 					auto& node = hierarchy._m_nodes.emplace_back();
-					node.name = in.get_line(false);
+					node.name = chunk.get_line(false);
 
-					node.parent_index = in.get_short();
+					node.parent_index = chunk.get_short();
 					node.parent = node.parent_index == -1 ? nullptr : &hierarchy._m_nodes[node.parent_index];
 
 					// NOTE: ZENGIN matrices are ROW MAJOR while GLM matrices are COLUMN MAJOR
-					node.transform = glm::transpose(glm::mat4 {in.get_vec4(), in.get_vec4(), in.get_vec4(), in.get_vec4()});
+					node.transform = glm::transpose(
+					    glm::mat4 {chunk.get_vec4(), chunk.get_vec4(), chunk.get_vec4(), chunk.get_vec4()});
 				}
 
-				hierarchy._m_bbox[0] = in.get_vec3();
-				hierarchy._m_bbox[1] = in.get_vec3();
-				hierarchy._m_collision_bbox[0] = in.get_vec3();
-				hierarchy._m_collision_bbox[1] = in.get_vec3();
-				hierarchy._m_root_translation = in.get_vec3();
+				hierarchy._m_bbox[0] = chunk.get_vec3();
+				hierarchy._m_bbox[1] = chunk.get_vec3();
+				hierarchy._m_collision_bbox[0] = chunk.get_vec3();
+				hierarchy._m_collision_bbox[1] = chunk.get_vec3();
+				hierarchy._m_root_translation = chunk.get_vec3();
 
-				(void) /* checksum = */ in.get_uint();
+				(void) /* checksum = */ chunk.get_uint();
 				break;
 			}
 			case hierarchy_chunk::stats:
 				// maybe a date?
-				in.skip(16);
-				(void) /* path? = */ in.get_line(false);
+				chunk.skip(16);
+				(void) /* path? = */ chunk.get_line(false);
 				break;
 			case hierarchy_chunk::end:
 				end_hierarchy = true;
@@ -57,13 +57,8 @@ namespace phoenix {
 				break;
 			}
 
-			if (in.position() != end) {
-				fmt::print(stderr,
-				           "warning: model hierarchy: not all data or too much data consumed from section 0x{:X}\n",
-				           chunk);
-			}
-
-			in.position(end);
+			if (chunk.remaining() != 0)
+				fmt::print(stderr, "warning: model hierarchy: not all data consumed from section 0x{:X}\n", type);
 		} while (!end_hierarchy);
 
 		return hierarchy;
