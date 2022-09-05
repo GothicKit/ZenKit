25a4fbfeab34e227a19d0785ac2b3e04936df482
Luis Michaelis <me@lmichaelis.de>
2022-09-05 18:10:54 +0200

    font: add real constructor and documentation

--- include/phoenix/font.hh
+++ include/phoenix/font.hh
@@ -8,76 +8,92 @@
 #include <vector>
 
 namespace phoenix {
-	/**
-	 * @brief Represents one glyph.
-	 */
+	/// \brief A single font glyph.
 	struct glyph {
+		/// \brief The width of the glyph in pixels.
 		std::uint8_t width;
+
+		/// \brief The position of the top left and bottom right corners of the glyph in the font texture.
+		/// \note These values are not stored as absolute pixels but rather in percent of the width and
+		///       height of the image. Thus to calculate the real pixel position of the top right corner,
+		///       one multiplies `uv[0].x` by the width of the font texture and `uv[0].y` by its height.
 		glm::vec2 uv[2];
 	};
 
-	/**
-	 * @brief Represents a font file (.FNT).
-	 *
-	 * Parses ZenGin font files. The reference implementation can be found on GitHub:
-	 * https://github.com/Try/ZenLib/blob/732077c82589f5060d1762839293b996c8222c18/zenload/zCFont.cpp and
-	 * the original version by Andre Taulien was also referenced:
-	 * https://github.com/ataulien/ZenLib/blob/e1a5e1b12e71690a5470f3be2aa3d0d6419f5191/zenload/zCFont.cpp
-	 *
-	 * Thanks to the original authors, Andre Taulien and kapitaenzufall as well as Try for additional work on their
-	 * ZenLib fork!
-	 *
-	 * @see https://github.com/Try/ZenLib
-	 * @see https://github.com/ataulien/ZenLib
-	 */
+	/// \brief Represents a *ZenGin* font file.
+	///
+	/// <p>Fonts in the *ZenGin* consist of a font definition file and a font texture. This class represents the former.
+	/// Font definition files contain a set of glyphs which define the extents of a
+	/// [Windows-1252](https://en.wikipedia.org/wiki/Windows-1252) encoded character within the font texture file. Font
+	/// files can be identified most easily by their `.FNT` extension or alternatively through the `"1\n"` string at the
+	/// beginning of the file.</p>
 	class font {
 	public:
-		/**
-		 * @brief Parses in a font from the given reader.
-		 * @param in The reader to read from
-		 * @return The font parsed.
-		 * @throws parser_error if parsing fails.
-		 */
-		[[nodiscard]] static font parse(buffer& in);
+		/// \brief Creates a new font from the given values.
+		/// \param name The name of the font.
+		/// \param height The height of each glyph in pixels.
+		/// \param glyphs A list of glyphs of this font. There should always be 256 glyphs in a font.
+		/// \warning While *phoenix* supports an arbitrary number of glyphs for fonts, Gothic and Gothic II always
+		///          expect 256 glyphs for all fonts. Should you create a font a number of glyphs not equal to 256 and
+		///          try to load it into *ZenGin*, it will fail.
+		font(std::string name, std::uint32_t height, std::vector<glyph> glyphs);
 
-		/**
-		 * @brief Parses in a font from the given reader.
-		 * @param in The reader to read from
-		 * @return The font parsed.
-		 * @throws parser_error if parsing fails.
-		 */
+		/// \brief Parses a font from the data in the given reader.
+		///
+		/// <p>This implementation is heavily based on the implementation found in
+		/// [ZenLib](https://github.com/Try/ZenLib). The only change to the parsing logic is, that this implementation
+		/// dynamically allocates the number of glyphs from the glyph count stored within the font file. That change is
+		/// incompatible with the original *ZenGin*.</p>
+		///
+		/// \param[in,out] buf The buffer to read from.
+		/// \return The parsed font object.
+		/// \note After this function returns the position of \p buf will be at the end of the parsed object.
+		///       If you would like to keep your buffer immutable, consider passing a copy of it to #parse(buffer&&)
+		///       using buffer::duplicate.
+		/// \throws parser_error if parsing fails.
+		/// \see #parse(buffer&&)
+		[[nodiscard]] static font parse(buffer& buf);
+
+		/// \brief Parses a font from the data in the given reader.
+		/// \param[in] buf The buffer to read from (by rvalue-reference).
+		/// \return The parsed font object.
+		/// \throws parser_error if parsing fails.
+		/// \see #parse(buffer&)
 		[[nodiscard]] inline static font parse(buffer&& in) {
-			return parse(in);
+			return font::parse(in);
 		}
 
-		/**
-		 * @return The name of the font
-		 */
+		/// \brief Returns the name of this font.
+		/// \return The name of this font.
 		[[nodiscard]] inline const std::string& name() const noexcept {
 			return _m_name;
 		}
 
-		/**
-		 * @return The height of the font
-		 */
+		/// \brief Returns the height of glyphs of this font in pixels.
+		/// \return The height of glyphs of this font in pixels.
 		[[nodiscard]] inline std::uint32_t height() const noexcept {
 			return _m_height;
 		}
 
-		/**
-		 * @return The glyphs in the font (length of FONT_MAX_GLYPHS)
-		 */
+		/// \brief Returns all glyphs of this font.
+		///
+		/// <p>*ZenGin* fonts contain characters present in the [Windows-1252 character
+		/// encoding](https://en.wikipedia.org/wiki/Windows-1252) which is generally used by *Gothic* and *Gothic II*.
+		/// The returned vector contains these glyphs in order.</p>
+		///
+		/// <p>Some glyphs may not actually be present in the font texture. For those glyphs, the stored UV-coordinates
+		/// will be invalid in some way (ie. they may point to a pixel not actually in the texture or be negative).</p>
+		///
+		/// \note The glyphs UV-coordinates are not straightforward. Refer to glyph::uv for an explanation about to
+		///       how to use them
+		/// \return All glyphs of this font.
 		[[nodiscard]] inline const std::vector<glyph>& glyphs() const noexcept {
 			return _m_glyphs;
 		}
-
-	protected:
-		font() = default;
 
 	private:
 		std::string _m_name;
 		std::uint32_t _m_height;
 		std::vector<glyph> _m_glyphs;
 	};
-
 } // namespace phoenix
--- source/font.cc
+++ source/font.cc
@@ -5,32 +5,39 @@
 #include <fmt/format.h>
 
 namespace phoenix {
+	font::font(std::string name, std::uint32_t height, std::vector<glyph> glyphs)
+	    : _m_name(std::move(name)), _m_height(height), _m_glyphs(std::move(glyphs)) {}
+
 	font font::parse(buffer& in) {
 		try {
-			font fnt {};
-
 			auto version = in.get_line();
 			if (version != "1") {
 				throw parser_error {"font", fmt::format("version mismatch: expected version '1', got '{}'", version)};
 			}
 
-			fnt._m_name = in.get_line(false);
-			fnt._m_height = in.get_uint();
-			fnt._m_glyphs.resize(in.get_uint());
+			auto name = in.get_line(false);
+			auto height = in.get_uint();
 
-			for (auto& _m_glyph : fnt._m_glyphs) {
-				_m_glyph.width = in.get();
+			std::vector<glyph> glyphs {};
+			glyphs.resize(in.get_uint());
+
+			for (auto& glyph : glyphs) {
+				glyph.width = in.get();
 			}
 
-			for (auto& _m_glyph : fnt._m_glyphs) {
-				_m_glyph.uv[0] = in.get_vec2();
+			for (auto& glyph : glyphs) {
+				glyph.uv[0] = in.get_vec2();
 			}
 
-			for (auto& _m_glyph : fnt._m_glyphs) {
-				_m_glyph.uv[1] = in.get_vec2();
+			for (auto& glyph : glyphs) {
+				glyph.uv[1] = in.get_vec2();
 			}
 
-			return fnt;
+			return font {
+			    name,
+			    height,
+			    std::move(glyphs),
+			};
 		} catch (const buffer_error& exc) {
 			throw parser_error {"font", exc, "eof reached"};
 		}
