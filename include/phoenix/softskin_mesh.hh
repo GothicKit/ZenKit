85357fdfcbaca76f22942cceec31ddef87943ec2
Luis Michaelis <me@lmichaelis.de>
2022-09-08 18:10:30 +0200

    softskin_mesh: update documentation

--- include/phoenix/softskin_mesh.hh
+++ include/phoenix/softskin_mesh.hh
@@ -6,7 +6,6 @@
 #include <phoenix/proto_mesh.hh>
 
 namespace phoenix {
-
 	struct wedge_normal {
 		glm::vec3 normal;
 		std::uint32_t index;
@@ -18,58 +17,49 @@ namespace phoenix {
 		std::uint8_t node_index;
 	};
 
-	/**
-	 * @brief Represents a rigged mesh.
-	 *
-	 * Parses ZenGin meshes. The reference implementation can be found on GitHub:
-	 * https://github.com/Try/ZenLib/blob/9b94299307aebcd99887c5e0b050a13622e41ecb/zenload/zCMeshSoftSkin.cpp and the
-	 * original version by Andre Taulien was also referenced:
-	 * https://github.com/ataulien/ZenLib/blob/e1a5e1b12e71690a5470f3be2aa3d0d6419f5191/zenload/zCMeshSoftSkin.cpp
-	 *
-	 * Thanks to the original author, Andre Taulien as well as Try for additional work on their ZenLib fork!
-	 *
-	 * @see https://github.com/ataulien/ZenLib
-	 * @see https://github.com/Try/ZenLib
-	 */
+	/// \brief Represents a soft-skin mesh.
 	class softskin_mesh {
 	public:
+		/// \brief Parses a soft-skin mesh from the data in the given buffer.
+		/// \param[in,out] buf The buffer to read from.
+		/// \return The parsed soft-skin mesh.
+		/// \note After this function returns the position of \p buf will be at the end of the parsed object.
+		///       If you would like to keep your buffer immutable, consider passing a copy of it to #parse(buffer&&)
+		///       using buffer::duplicate.
+		/// \throws parser_error if parsing fails.
+		/// \see #parse(buffer&&)
 		[[nodiscard]] static softskin_mesh parse(buffer& in);
 
+		/// \brief Parses a soft-skin mesh from the data in the given buffer.
+		/// \param[in] buf The buffer to read from (by rvalue-reference).
+		/// \return The parsed soft-skin mesh.
+		/// \throws parser_error if parsing fails.
+		/// \see #parse(buffer&)
 		[[nodiscard]] inline static softskin_mesh parse(buffer&& in) {
-			return parse(in);
+			return softskin_mesh::parse(in);
 		}
 
-		/**
-		 * @return The embedded proto-mesh.
-		 */
+		/// \return The embedded proto-mesh.
 		[[nodiscard]] const proto_mesh& mesh() const noexcept {
 			return _m_mesh;
 		}
 
-		/**
-		 * @return The meshes bounding boxes (there is one for each node).
-		 */
+		/// \return The meshes bounding boxes (there is one for each node).
 		[[nodiscard]] const std::vector<obb>& bboxes() const noexcept {
 			return _m_bboxes;
 		}
 
-		/**
-		 * @return Egde normals.
-		 */
+		/// \return A list of wedge normals.
 		[[nodiscard]] const std::vector<wedge_normal>& wedge_normals() const noexcept {
 			return _m_wedge_normals;
 		}
 
-		/**
-		 * @return Node weights.
-		 */
+		/// \return Node weights.
 		[[nodiscard]] const std::vector<std::vector<weight_entry>>& weights() const noexcept {
 			return _m_weights;
 		}
 
-		/**
-		 * @return Nodes.
-		 */
+		/// \return Nodes.
 		[[nodiscard]] const std::vector<std::int32_t>& nodes() const noexcept {
 			return _m_nodes;
 		}
