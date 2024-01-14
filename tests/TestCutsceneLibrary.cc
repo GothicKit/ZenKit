76d79c63e0029aaa41dd140ca84da2effc175105
Luis Michaelis <me@lmichaelis.de>
2024-01-14 09:14:19 +0100

    test(CutsceneLibrary): add save-load test

--- tests/TestCutsceneLibrary.cc
+++ tests/TestCutsceneLibrary.cc
@@ -6,35 +6,38 @@
 
 #include <fstream>
 
+static void verify_g1(zenkit::CutsceneLibrary const& msgs) {
+	CHECK_EQ(msgs.blocks.size(), 7360);
+
+	auto* msg20 = msgs.block_by_name("DIA_ARTO_PERM_15_00");
+	auto* msg100 = msgs.block_by_name("DIA_BaalKagan_WasDrin_13_01");
+	auto* msg200 = &msgs.blocks[200];
+	auto* msg_none = msgs.block_by_name("nonexistent");
+
+	CHECK_NE(msg20, nullptr);
+	CHECK_NE(msg100, nullptr);
+	CHECK_NE(msg200, nullptr);
+	CHECK_EQ(msg_none, nullptr);
+
+	CHECK_EQ(msg20->message.type, 0);
+	CHECK_EQ(msg20->message.text, "Du redest nicht viel, was?");
+	CHECK_EQ(msg20->message.name, "DIA_ARTO_PERM_15_00.WAV");
+
+	CHECK_EQ(msg100->message.type, 0);
+	CHECK_EQ(msg100->message.text, "Ich kann dich auf viele Arten entlohnen.");
+	CHECK_EQ(msg100->message.name, "DIA_BAALKAGAN_WASDRIN_13_01.WAV");
+
+	CHECK_EQ(msg200->message.type, 0);
+	CHECK_EQ(msg200->message.text, "Stimmt genau.");
+	CHECK_EQ(msg200->message.name, "DIA_BAALTARAN_INTOCASTLE_EXACTLY_15_00.WAV");
+}
+
 TEST_SUITE("CutsceneLibrary") {
 	TEST_CASE("CutsceneLibrary.load(GOTHIC1)") {
 		auto r = zenkit::Read::from("./samples/ou.proprietary.bin");
 		zenkit::CutsceneLibrary msgs {};
 		msgs.load(r.get());
-
-		CHECK_EQ(msgs.blocks.size(), 7360);
-
-		auto* msg20 = msgs.block_by_name("DIA_ARTO_PERM_15_00");
-		auto* msg100 = msgs.block_by_name("DIA_BaalKagan_WasDrin_13_01");
-		auto* msg200 = &msgs.blocks[200];
-		auto* msg_none = msgs.block_by_name("nonexistent");
-
-		CHECK_NE(msg20, nullptr);
-		CHECK_NE(msg100, nullptr);
-		CHECK_NE(msg200, nullptr);
-		CHECK_EQ(msg_none, nullptr);
-
-		CHECK_EQ(msg20->message.type, 0);
-		CHECK_EQ(msg20->message.text, "Du redest nicht viel, was?");
-		CHECK_EQ(msg20->message.name, "DIA_ARTO_PERM_15_00.WAV");
-
-		CHECK_EQ(msg100->message.type, 0);
-		CHECK_EQ(msg100->message.text, "Ich kann dich auf viele Arten entlohnen.");
-		CHECK_EQ(msg100->message.name, "DIA_BAALKAGAN_WASDRIN_13_01.WAV");
-
-		CHECK_EQ(msg200->message.type, 0);
-		CHECK_EQ(msg200->message.text, "Stimmt genau.");
-		CHECK_EQ(msg200->message.name, "DIA_BAALTARAN_INTOCASTLE_EXACTLY_15_00.WAV");
+		verify_g1(msgs);
 	}
 
 	TEST_CASE("CutsceneLibrary.dump(GOTHIC1)") {
@@ -50,29 +53,7 @@ TEST_SUITE("CutsceneLibrary") {
 		zenkit::CutsceneLibrary msgs {};
 		msgs.load(r.get());
 
-		CHECK_EQ(msgs.blocks.size(), 7360);
-
-		auto* msg20 = msgs.block_by_name("DIA_ARTO_PERM_15_00");
-		auto* msg100 = msgs.block_by_name("DIA_BaalKagan_WasDrin_13_01");
-		auto* msg200 = &msgs.blocks[200];
-		auto* msg_none = msgs.block_by_name("nonexistent");
-
-		CHECK_NE(msg20, nullptr);
-		CHECK_NE(msg100, nullptr);
-		CHECK_NE(msg200, nullptr);
-		CHECK_EQ(msg_none, nullptr);
-
-		CHECK_EQ(msg20->message.type, 0);
-		CHECK_EQ(msg20->message.text, "Du redest nicht viel, was?");
-		CHECK_EQ(msg20->message.name, "DIA_ARTO_PERM_15_00.WAV");
-
-		CHECK_EQ(msg100->message.type, 0);
-		CHECK_EQ(msg100->message.text, "Ich kann dich auf viele Arten entlohnen.");
-		CHECK_EQ(msg100->message.name, "DIA_BAALKAGAN_WASDRIN_13_01.WAV");
-
-		CHECK_EQ(msg200->message.type, 0);
-		CHECK_EQ(msg200->message.text, "Stimmt genau.");
-		CHECK_EQ(msg200->message.name, "DIA_BAALTARAN_INTOCASTLE_EXACTLY_15_00.WAV");
+		verify_g1(msgs);
 	}
 
 	TEST_CASE("CutsceneLibrary.load(GOTHIC2)" * doctest::skip()) {
