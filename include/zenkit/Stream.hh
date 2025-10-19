8c4edf75ebdda1fa27c863f5fae81b3744f7187f
Luis Michaelis <me@lmichaelis.de>
2025-10-19 17:11:43 +0200

    fix(Stream): fix implicit `ssize_t` cast issues with some compilers

--- include/zenkit/Stream.hh
+++ include/zenkit/Stream.hh
@@ -217,7 +217,7 @@ namespace zenkit {
 
 			cb(w);
 
-			auto len = static_cast<ssize_t>(w->tell()) - size_off - sizeof(uint32_t);
+			auto len = static_cast<ssize_t>(w->tell()) - size_off - static_cast<ssize_t>(sizeof(uint32_t));
 			w->seek(size_off, Whence::BEG);
 			w->write_uint(static_cast<uint32_t>(len));
 			w->seek(len, Whence::CUR);
